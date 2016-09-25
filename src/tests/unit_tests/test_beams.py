f09429705d04a77d17898f12bed58227014cede3
Marcin <muszynskimarcin@wp.pl>
2016-09-25 17:11:01 +0200

    Beam unit tests

--- src/tests/unit_tests/test_beams.py
+++ src/tests/unit_tests/test_beams.py
@@ -12,20 +12,19 @@ class TestBeams(TestCase):
         self.node_3 = Node.get_or_create(x=4, y=0)
         self.section = Section.get_or_create(name='test_section', area=10, inertia=100)
         self.material = Material.get_or_create(name='test_material', young=10000, poisson=0.4)
-        self.beam_1 = Beam(
+        self.beam_1 = Beam.get_or_create(
             start_node=self.node_1,
             end_node=self.node_2,
             section=self.section,
             material=self.material
         )
-        self.beam_2 = Beam(
-            start_node=self.node_2,
+        self.beam_2 = Beam.get_or_create(
+            start_node=self.node_1,
             end_node=self.node_3,
             section=self.section,
             material=self.material,
         )
 
-    @skip('For now')
     def test_is_beam_instance(self):
         self.assertIsInstance(self.beam_1, Beam)
         self.assertIsInstance(self.beam_2, Beam)
@@ -33,6 +32,9 @@ class TestBeams(TestCase):
     def test_beams_params(self):
         self.assertEqual(self.beam_1.section.area, 10)
         self.assertEqual(self.beam_2.material.poisson, 0.4)
+
+    def test_beam_length(self):
+        self.assertEqual(4, self.beam_2.length())
 
 if __name__ == '__main__':
     main()
